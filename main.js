7c2bd83069d00eb9da09091df992e113bfc6856d
Trustacean <pizzacrustacean15@gmail.com>
2024-01-14 10:45:02 +0700

    feat: rings rotates faster upon loading then slow back down

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 window.onload = function () {
     let card = document.querySelector('.card');
     let profile = document.querySelector('.profile-info__section');
-    
-    var isAnimating = false;
-    
-    document.addEventListener('click', function () {
-        if (isAnimating) {
-            return;
-        }
-        
-        isAnimating = true;
-        
-        var rings = document.querySelectorAll('[class^="ring"]');
-        
-        rings.forEach(function (ring, index) {
-            card.style.transition = 'transform 1.5s, box-shadow 1.5s';
-            ring.style.borderRadius = '47%';
-            ring.style.opacity = '0.8';
-            ring.style.filter = 'hue-rotate(-50deg)';
-            card.style.filter = 'hue-rotate(-25deg)';
-            
-            setTimeout(function () {
-                ring.style.borderRadius = '50%';
-                ring.style.opacity = '0.3';
-                ring.style.filter = 'hue-rotate(0deg)';
-                card.style.filter = 'hue-rotate(0deg)';
-                
-                if (index === rings.length - 1) {
-                    isAnimating = false;
-                }
-            }, 1500);
-            card.style.transition = 'transform 0.9s, box-shadow 0.9s';
+
+    setTimeout(function () {
+
+        var isAnimating = false;
+
+        document.addEventListener('click', function () {
+            if (isAnimating) {
+                return;
+            }
+
+            isAnimating = true;
+
+            var rings = document.querySelectorAll('[class^="ring"]');
+
+            rings.forEach(function (ring, index) {
+                card.style.transition = 'transform 1.5s, box-shadow 1.5s';
+                ring.style.borderRadius = '47%';
+                ring.style.opacity = '0.8';
+                ring.style.filter = 'hue-rotate(-50deg)';
+                card.style.filter = 'hue-rotate(-25deg)';
+
+                setTimeout(function () {
+                    ring.style.borderRadius = '50%';
+                    ring.style.opacity = '0.3';
+                    ring.style.filter = 'hue-rotate(0deg)';
+                    card.style.filter = 'hue-rotate(0deg)';
+
+                    if (index === rings.length - 1) {
+                        isAnimating = false;
+                    }
+                }, 1500);
+                card.style.transition = 'transform 0.9s, box-shadow 0.9s';
+            });
         });
-    });
-    
+    }, 6000);
+
     document.onmousemove = function (e) {
-        
-        let X = e.clientX - window.innerWidth /2;
-        let Y = e.clientY - window.innerHeight /2;
-        
+
+        let X = e.clientX - window.innerWidth / 2;
+        let Y = e.clientY - window.innerHeight / 2;
+
         let parallaxX = -(X / 50);
         let parallaxY = -(Y / 50);
         let parallaxBoxShadow = `${parallaxX / 4}px ${parallaxY / 4}px 0px rgba(77, 166, 255, 0.7)`;
