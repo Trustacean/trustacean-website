02f62dffacf7e88027987e4c0d5af1e364c61e31
Trustacean <pizzacrustacean15@gmail.com>
2024-01-15 23:38:27 +0700

    fix: animation optimizations

--- main.js
+++ main.js
@@ -16,13 +16,10 @@ window.onload = function () {
             var rings = document.querySelectorAll('[class^="ring"]');
 
             rings.forEach(function (ring, index) {
-                ring.style.borderRadius = '49%';
                 ring.style.opacity = '0.8';
                 ring.style.filter = 'hue-rotate(-40deg)';
-                ring.style.transform = 'rotate(20deg)'
 
                 setTimeout(function () {
-                    ring.style.borderRadius = '50%';
                     ring.style.opacity = '0.3';
                     ring.style.filter = 'hue-rotate(0deg)';
 
@@ -33,7 +30,7 @@ window.onload = function () {
             });
         });
     }, 6000);
-
+    
     document.onmousemove = function (e) {
 
         let X = e.clientX - window.innerWidth / 2;
@@ -41,14 +38,12 @@ window.onload = function () {
 
         let parallaxX = -(X / 50);
         let parallaxY = -(Y / 50);
-        let parallaxBoxShadow = `${parallaxX / 4}px ${parallaxY / 4}px 0px rgba(77, 166, 255, 0.7)`;
 
         if (card.matches(':hover')) {
             card.style.transform = `scale(1.1)`;
         } else {
             card.style.transform = `translate(${parallaxX}px, ${parallaxY}px)`;
         }
-        card.style.boxShadow = parallaxBoxShadow;
-        // profile.style.transform = `translate(${parallaxX / 3}px, ${parallaxY / 3}px)`;
+        profile.style.transform = `translate(${parallaxX / 3}px, ${parallaxY / 3}px)`;
     };
 };
